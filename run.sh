4e6f9dc4e55927e02241f1374dbf0f94cd91a6ce
anandukch <anandukch@gmail.com>
2023-05-22 19:57:44 +0530

    updated run script

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 
 
 # if [[ "$DEBUG" = "True" ]]; then
-#     uvicorn app.main:app --root-path ${BASE_PATH:-""} --port ${PORT:-8000} --host 0.0.0.0 --reload 
+uvicorn app.main:app --root-path ${BASE_PATH:-""} --port ${PORT:-8000} --host 0.0.0.0 --reload 
 # else
 #     uvicorn app.main:app --root-path ${BASE_PATH:-""} --port ${PORT:-8000} --host 0.0.0.0
 # fi
 
-export APP_MODULE=${APP_MODULE-app.main:app}
-export HOST=${HOST:-0.0.0.0}
-export PORT=${PORT:-8001}
+# export APP_MODULE=${APP_MODULE-app.main:app}
+# export HOST=${HOST:-0.0.0.0}
+# export PORT=${PORT:-8001}
 
-exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
+# exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
